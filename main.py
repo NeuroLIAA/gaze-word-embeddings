40cdf52d2abdfc814018307beb1dd14982e3cca7
FerminT <fermintravi@gmail.com>
2023-08-17 16:38:37 -0300

    Add w2v training

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import argparse
 from pathlib import Path
+from gensim.models import Word2Vec
+
+
+def train(corpus, model_path):
+    model = Word2Vec(sentences=corpus, vector_size=100, window=5, min_count=5, workers=4)
+    model.save(str(model_path))
 
 
 def load_corpus(path):
@@ -18,8 +24,10 @@ def load_corpus(path):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--corpus', type=str, default='corpus', help='Path to training corpus')
+    parser.add_argument('-m', '--model', type=str, default='w2v', help='Model name')
+    parser.add_argument('-o', '--output', type=str, default='model', help='Where to save trained model')
     args = parser.parse_args()
 
-    corpus_path = Path(args.corpus)
+    corpus_path, model_path = Path(args.corpus), Path(args.output, args.model)
     corpus = load_corpus(corpus_path)
-    print(corpus)
+    train(corpus, model_path)
