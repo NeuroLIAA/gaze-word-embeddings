b335ca4ec379135a8320be6d557020db677dbf0b
FerminT <fermintravi@gmail.com>
2023-08-23 18:40:14 -0300

    Refactor

--- main.py
+++ main.py
@@ -16,23 +16,24 @@ def train(corpus, file_path):
     return model
 
 
-def load_baseline_corpus(dataset='large_spanish_corpus', name='all_wikis', split='10%'):
+def load_from_spanish_corpus(dataset='large_spanish_corpus', name='all_wikis', split='10%'):
     baseline_corpus = load_dataset(dataset, name, split=f'train[:{split}]')['text']
     baseline_corpus = [preprocess_str(text, chars_mapping) for text in baseline_corpus if len(text) > 0]
     return baseline_corpus
 
 
-def load_corpus(name, chars_mapping, split='10%'):
-    if not Path(name).exists():
-        corpus = load_baseline_corpus(name=name, split=split)
+def load_corpus(path, chars_mapping, split='10%'):
+    if not path.exists():
+        corpus = load_from_spanish_corpus(name=path.name, split=split)
     else:
-        files = [f for f in Path(name).iterdir()]
+        files = [f for f in path.iterdir()]
         corpus = []
         for file in files:
             if file.is_file():
                 with file.open('r') as f:
                     sentences = f.read().split('.')
-                    corpus.extend([preprocess_str(sentence, chars_mapping) for sentence in sentences if len(sentence) > 0])
+                    corpus.extend([preprocess_str(sentence, chars_mapping)
+                                   for sentence in sentences if len(sentence) > 0])
             elif file.is_dir():
                 corpus += load_corpus(file, chars_mapping)
     return corpus
@@ -46,7 +47,7 @@ def preprocess_str(string, chars_mapping):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--corpora', type=str, default='all_wikis+texts',
-                        help='  Texts to be employed for training')
+                        help='Texts to be employed for training')
     parser.add_argument('-m', '--model', type=str, default='wikis_texts', help='Model name')
     parser.add_argument('-s', '--split', type=str, default='10%', help='Split for baseline corpus')
     parser.add_argument('-o', '--output', type=str, default='model', help='Where to save the trained model')
@@ -57,5 +58,5 @@ if __name__ == '__main__':
     corpora = args.corpora.split('+')
     training_corpus = []
     for corpus_name in corpora:
-        training_corpus += load_corpus(corpus_name, chars_mapping, args.split)
+        training_corpus += load_corpus(Path(corpus_name), chars_mapping, args.split)
     train(training_corpus, model_path)
