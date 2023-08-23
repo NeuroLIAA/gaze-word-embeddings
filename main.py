b641f22a2011c248a4e1928198393a2f5d634616
FerminT <fermintravi@gmail.com>
2023-08-23 18:37:01 -0300

    Incrementally add corpus from command line argument

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ CHARS_MAP = {'—': '', '‒': '', '−': '', '-': '', '«': '', '»': '',
 
 
 def train(corpus, file_path):
-    model = Word2Vec(sentences=corpus, vector_size=100, window=5, min_count=5, workers=-1)
+    model = Word2Vec(sentences=corpus, vector_size=100, window=5, min_count=1, workers=-1)
     model.save(str(file_path))
     return model
 
@@ -22,16 +22,19 @@ def load_baseline_corpus(dataset='large_spanish_corpus', name='all_wikis', split
     return baseline_corpus
 
 
-def load_corpus(path, chars_mapping):
-    files = [f for f in path.iterdir()]
-    corpus = []
-    for file in files:
-        if file.is_file():
-            with file.open('r') as f:
-                sentences = f.read().split('.')
-                corpus.extend([preprocess_str(sentence, chars_mapping) for sentence in sentences if len(sentence) > 0])
-        elif file.is_dir():
-            corpus += load_corpus(file, chars_mapping)
+def load_corpus(name, chars_mapping, split='10%'):
+    if not Path(name).exists():
+        corpus = load_baseline_corpus(name=name, split=split)
+    else:
+        files = [f for f in Path(name).iterdir()]
+        corpus = []
+        for file in files:
+            if file.is_file():
+                with file.open('r') as f:
+                    sentences = f.read().split('.')
+                    corpus.extend([preprocess_str(sentence, chars_mapping) for sentence in sentences if len(sentence) > 0])
+            elif file.is_dir():
+                corpus += load_corpus(file, chars_mapping)
     return corpus
 
 
@@ -42,17 +45,17 @@ def preprocess_str(string, chars_mapping):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--corpus', type=str, default='corpus', help='Path to training corpus')
-    parser.add_argument('-m', '--model', type=str, default='w2v', help='Model name')
-    parser.add_argument('-o', '--output', type=str, default='model', help='Where to save the trained model')
-    parser.add_argument('-d', '--dataset', type=str, default='all_wikis',
-                        help='Dataset name of baseline corpus')
+    parser.add_argument('-c', '--corpora', type=str, default='all_wikis+texts',
+                        help='  Texts to be employed for training')
+    parser.add_argument('-m', '--model', type=str, default='wikis_texts', help='Model name')
     parser.add_argument('-s', '--split', type=str, default='10%', help='Split for baseline corpus')
+    parser.add_argument('-o', '--output', type=str, default='model', help='Where to save the trained model')
     args = parser.parse_args()
+    model_path = Path(args.output, args.model)
 
-    corpus_path, model_path, baseline_path = (Path(args.corpus), Path(args.output, args.model),
-                                              Path(args.output, 'baseline'))
     chars_mapping = str.maketrans(CHARS_MAP)
-    baseline_corpus = load_baseline_corpus(name=args.dataset, split=args.split)
-    corpus = load_corpus(corpus_path, chars_mapping)
-    train(baseline_corpus + corpus, model_path)
+    corpora = args.corpora.split('+')
+    training_corpus = []
+    for corpus_name in corpora:
+        training_corpus += load_corpus(corpus_name, chars_mapping, args.split)
+    train(training_corpus, model_path)
