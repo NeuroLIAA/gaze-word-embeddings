944bd910484a996d5eadf63efa8ab7deaf79a2e3
FerminT <fermintravi@gmail.com>
2023-08-23 15:21:19 -0300

    Preprocess corpus and divide into words

--- main.py
+++ main.py
@@ -1,37 +1,48 @@
 import argparse
 from pathlib import Path
 from gensim.models import Word2Vec
+from gensim.parsing import preprocessing
 from datasets import load_dataset
+
+CHARS_MAP = {'—': '', '‒': '', '−': '', '-': '', '«': '', '»': '',
+             '“': '', '”': '', '\'': '', '\"': '', '‘': '', '’': '',
+             '(': '', ')': '', ';': '', ',': '', ':': '', '.': '', '…': '',
+             '¿': '', '?': '', '¡': '', '!': ''}
 
 
 def train(corpus, model_path):
-    model = Word2Vec(sentences=corpus, vector_size=100, window=5, min_count=5, workers=4)
+    model = Word2Vec(sentences=corpus, vector_size=100, window=5, min_count=5, workers=-1)
     model.save(str(model_path))
     return model
 
 
 def load_baseline_corpus(dataset='large_spanish_corpus', name='all_wikis', split='10%'):
-    baseline_corpus = load_dataset(dataset, name, split=f'train[:{split}]', streaming=True)
+    baseline_corpus = load_dataset(dataset, name, split=f'train[:{split}]')['text']
     return baseline_corpus
 
 
-def load_corpus(path):
+def load_corpus(path, chars_mapping):
     files = [f for f in path.iterdir()]
     corpus = []
     for file in files:
         if file.is_file():
             with file.open('r') as f:
-                corpus.append(f.read())
+                corpus.append(preprocess_str(f.read(), chars_mapping))
         elif file.is_dir():
-            corpus += load_corpus(file)
+            corpus += load_corpus(file, chars_mapping)
     return corpus
+
+
+def preprocess_str(string, chars_mapping):
+    string = string.translate(chars_mapping)
+    return preprocessing.split_on_space(string.lower())
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--corpus', type=str, default='corpus', help='Path to training corpus')
     parser.add_argument('-m', '--model', type=str, default='w2v', help='Model name')
-    parser.add_argument('-o', '--output', type=str, default='model', help='Where to save trained model')
+    parser.add_argument('-o', '--output', type=str, default='model', help='Where to save the trained model')
     parser.add_argument('-d', '--dataset', type=str, default='all_wikis',
                         help='Dataset name of baseline corpus')
     parser.add_argument('-s', '--split', type=str, default='10%', help='Split for baseline corpus')
@@ -39,10 +50,12 @@ if __name__ == '__main__':
 
     corpus_path, model_path, baseline_path = (Path(args.corpus), Path(args.output, args.model),
                                               Path(args.output, 'baseline'))
+    chars_mapping = str.maketrans(CHARS_MAP)
     baseline_corpus = load_baseline_corpus(name=args.dataset, split=args.split)
     if not baseline_path.exists():
+        baseline_path.parent.mkdir(exist_ok=True)
         baseline = train(baseline_corpus, baseline_path)
     else:
         baseline = Word2Vec.load(str(baseline_path))
-    corpus = load_corpus(corpus_path)
+    corpus = load_corpus(corpus_path, chars_mapping)
     train(corpus, model_path)
