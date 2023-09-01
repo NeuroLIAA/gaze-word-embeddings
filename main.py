4090c8846d66cc9079d33a9b240ca1834a4a9924
FerminT <fermintravi@gmail.com>
2023-09-01 16:07:09 -0300

    Remove huggingface dependencies

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from gensim.models import Word2Vec
 from gensim.corpora.wikicorpus import WikiCorpus
 from gensim.parsing import preprocessing
-from datasets import load_dataset
 
 CHARS_MAP = {'—': '', '‒': '', '−': '', '-': '', '«': '', '»': '',
              '“': '', '”': '', '\'': '', '\"': '', '‘': '', '’': '',
@@ -26,19 +25,10 @@ def train(corpus, file_path):
     return model
 
 
-def load_from_spanish_corpus(dataset='large_spanish_corpus', name='all_wikis', split=0.1):
-    baseline_corpus = load_dataset(dataset, name, split=f'train[:{int(split*100)}%]')
-    baseline_corpus = baseline_corpus.map(lambda row: preprocess_str(row['text'], chars_mapping))
-    baseline_corpus = baseline_corpus.filter(lambda row: len(row['text']) > 0)
-    return baseline_corpus
-
-
 def load_corpus(path, chars_mapping, split):
-    if not path.exists():
-        corpus = load_from_spanish_corpus(name=path.name, split=split)
-    else:
+    corpus = {'text': []}
+    if path.exists():
         files = [f for f in path.iterdir()]
-        corpus = {'text': []}
         for file in files:
             if file.is_file():
                 with file.open('r') as f:
