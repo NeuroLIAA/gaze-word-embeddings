d720631c83cb9aa2b2721fa10b61ef5591eac1ab
FerminT <fermintravi@gmail.com>
2023-09-02 12:21:35 -0300

    Create Corpus class

---
+++ corpora.py
@@ -0,0 +1,42 @@
+from gensim.corpora.wikicorpus import WikiCorpus
+from gensim.parsing import preprocessing
+
+CHARS_MAP = {'—': '', '‒': '', '−': '', '-': '', '«': '', '»': '',
+             '“': '', '”': '', '\'': '', '\"': '', '‘': '', '’': '',
+             '(': '', ')': '', ';': '', ',': '', ':': '', '.': '', '…': '',
+             '¿': '', '?': '', '¡': '', '!': '', '=': ''}
+
+class Corpus:
+    def __init__(self, name, path, split):
+        self.name = name
+        self.split = split
+        self.chars_mapping = str.maketrans(CHARS_MAP)
+        self.corpus = self.load_corpus(path)
+
+    def load_corpus(self, path):
+        corpus = []
+        if self.name == 'wikidump':
+            corpus = WikiCorpus(path)
+        else:
+            if path.exists():
+                files = [f for f in path.iterdir()]
+                for file in files:
+                    if file.is_file():
+                        with file.open('r') as f:
+                            sentences = f.read().split('.')
+                            corpus.extend([self.preprocess_str(sentence)
+                                                   for sentence in sentences if len(sentence) > 0])
+                    elif file.is_dir():
+                        corpus += self.load_corpus(file)
+        return corpus
+
+    def preprocess_str(self, string):
+        string = string.translate(self.chars_mapping)
+        words = preprocessing.split_on_space(string.lower())
+        return words
+
+    def get_texts(self):
+        if self.name == 'wikidump':
+            return self.corpus.get_texts()
+        else:
+            return self.corpus
