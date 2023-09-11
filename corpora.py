f16ad9088beeb681d29b88a16e215b67234d59ec
FerminT <fermintravi@gmail.com>
2023-09-11 18:03:13 -0300

    Command line arguments for parametrization

--- corpora.py
+++ corpora.py
@@ -5,11 +5,15 @@ import regex as re
 
 
 class Corpora:
-    def __init__(self):
+    def __init__(self, min_token_len, max_token_len, min_sentence_len):
         self.corpora = []
+        self.min_token_len = min_token_len
+        self.max_token_len = max_token_len
+        self.min_sentence_len = min_sentence_len
 
     def add_corpus(self, name, source, fraction, is_large):
-        self.corpora.append(Corpus(name, source, fraction, is_large))
+        self.corpora.append(Corpus(name, source, fraction, is_large,
+                                   self.min_token_len, self.max_token_len, self.min_sentence_len))
 
     def __iter__(self):
         for sentence in chain.from_iterable(corpus.get_texts() for corpus in self.corpora):
@@ -17,18 +21,18 @@ class Corpora:
 
 
 class Corpus:
-    def __init__(self, name, source, fraction, is_large):
+    def __init__(self, name, source, fraction, is_large, min_token_len, max_token_len, min_sentence_len):
         self.name = name
         self.fraction = fraction
         self.is_large = is_large
-        self.corpus = self.load_corpus(source)
+        self.corpus = self.load_corpus(source, min_token_len, max_token_len, min_sentence_len)
 
-    def load_corpus(self, source):
+    def load_corpus(self, source, min_token_len, max_token_len, min_sentence_len):
         corpus = {'text': []}
         if source.name == 'huggingface':
-            corpus = load_hg_dataset(self.name)
+            corpus = load_hg_dataset(self.name, min_token_len, max_token_len, min_sentence_len)
         else:
-            self.load_local_corpus(source, corpus)
+            corpus = self.load_local_corpus(source, corpus, min_token_len, max_token_len)
         return corpus
 
     def get_texts(self):
@@ -37,27 +41,28 @@ class Corpus:
         else:
             return self.corpus
 
-    def load_local_corpus(self, source, corpus):
+    def load_local_corpus(self, source, corpus, min_token_len, max_token_len):
         if source.exists():
             files = [f for f in source.iterdir()]
             for file in files:
                 if file.is_file():
                     with file.open('r') as f:
                         sentences = f.read().split('.')
-                        corpus['text'].extend([preprocess_str({'text': sentence})['text']
+                        corpus['text'].extend([preprocess_str({'text': sentence}, min_token_len, max_token_len)['text']
                                                for sentence in sentences if len(sentence) > 0])
                 elif file.is_dir():
-                    corpus['text'] += self.load_corpus(file)['text']
+                    corpus['text'] += self.load_local_corpus(file, corpus, min_token_len, max_token_len)['text']
+        return corpus
 
 
-def load_hg_dataset(name):
+def load_hg_dataset(name, min_token_len, max_token_len, min_sentence_len):
     corpus = load_dataset('large_spanish_corpus', name=name, split='train', streaming=True)
-    corpus = corpus.map(preprocess_str)
-    corpus = corpus.filter(lambda row: len(row['text']) > 10)
+    corpus = corpus.map(lambda row: preprocess_str(row, min_token_len, max_token_len))
+    corpus = corpus.filter(lambda row: len(row['text']) > min_sentence_len)
     return corpus
 
 
-def preprocess_str(string):
+def preprocess_str(string, min_token_len, max_token_len):
     string['text'] = re.sub(r'[^ \nA-Za-zÀ-ÖØ-öø-ÿ/]+', '', string['text'])
-    string['text'] = simple_preprocess(string['text'], min_len=2, max_len=20)
+    string['text'] = simple_preprocess(string['text'], min_len=min_token_len, max_len=max_token_len)
     return string
--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from pathlib import Path
 from gensim.models import Word2Vec
 
 
-def train(corpus, file_path):
-    model = Word2Vec(sentences=corpus, vector_size=300, window=5, min_count=100, workers=-1)
+def train(corpus, vector_size, window_size, min_count, file_path):
+    model = Word2Vec(sentences=corpus, vector_size=vector_size, window=window_size, min_count=min_count, workers=-1)
     model.save(str(file_path))
     return model
 
@@ -19,15 +19,24 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--model', type=str, default='wikis_texts', help='Model name')
     parser.add_argument('-f', '--fraction', type=float, default=1.0,
                         help='Fraction of baseline corpus to employ for training')
+    parser.add_argument('-min', '--min_count', type=int, default=100, help='Minimum number of occurrences for a word')
+    parser.add_argument('-size', '--size', type=int, default=300, help='Size of the word vectors')
+    parser.add_argument('-w', '--window', type=int, default=5, help='Window size')
+    parser.add_argument('-min_token', '--min_token', type=int, default=2,
+                        help='Word min length, in tokens')
+    parser.add_argument('-max_token', '--max_token', type=int, default=20,
+                        help='Word max length, in tokens')
+    parser.add_argument('-min_length', '--min_length', type=int, default=10,
+                        help='Sentence min length, in tokens, for large scale corpora')
     parser.add_argument('-o', '--output', type=str, default='model', help='Where to save the trained model')
     args = parser.parse_args()
     model_path = Path(args.output, args.model)
 
     corpora = args.corpora.split('+')
-    training_corpora = Corpora()
+    training_corpora = Corpora(args.min_token, args.max_token, args.min_length)
     for corpus in corpora:
         is_large = 'texts' not in corpus
         source = Path(corpus) if not is_large else Path(args.source)
         training_corpora.add_corpus(corpus, source, args.fraction, is_large)
     model_path.parent.mkdir(exist_ok=True)
-    train(training_corpora, model_path)
+    train(training_corpora, args.size, args.window, args.min_count, model_path)
