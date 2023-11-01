425deb8f2a61152c6613623fcdab51b1b828260b
FerminT <fermintravi@gmail.com>
2023-11-01 19:03:19 -0300

    Training messages without callbacks

--- train.py
+++ train.py
@@ -1,30 +1,17 @@
 from corpora import Corpora
-from gensim.models import Word2Vec, callbacks
+from gensim.models import Word2Vec
 from pathlib import Path
-
-
-class TrainLogger(callbacks.CallbackAny2Vec):
-    def __init__(self, model_name, fraction=1.0):
-        self.model_name = model_name
-        self.fraction = fraction
-        self.epoch = 0
-
-    def on_train_begin(self, model):
-        print(f'Beginning training of model {self.model_name} with {int(self.fraction * 100)}% of baseline corpus')
-
-    def on_train_end(self, model):
-        print(f'Finished training model {self.model_name} with {int(self.fraction * 100)}% of baseline corpus')
 
 
 def train(corpora, source, fraction, repeats, min_token_len, max_token_len, min_sentence_len,
           vector_size, window_size, min_count, model_name, save_path):
     corpora = corpora.split('+')
     corpora = load_corpora(corpora, source, fraction, repeats, min_token_len, max_token_len, min_sentence_len)
-    train_logger = TrainLogger(model_name, fraction)
-    model = Word2Vec(sentences=corpora, vector_size=vector_size, window=window_size, min_count=min_count,
-                     callbacks=[train_logger], workers=-1)
+    print(f'Beginning training model {model_name} with {int(fraction * 100)}% of baseline corpus')
+    model = Word2Vec(sentences=corpora, vector_size=vector_size, window=window_size, min_count=min_count, workers=-1)
     save_path.mkdir(exist_ok=True, parents=True)
     model.save(str(save_path / f'{model_name}.model'))
+    print(f'Finished training model {model_name} with {int(fraction * 100)}% of baseline corpus')
     return model
 
 
