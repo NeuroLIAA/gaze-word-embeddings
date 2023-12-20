fef5c84df2523fcebdffed281bb58ce78508db35
FerminT <fermintravi@gmail.com>
2023-12-20 12:31:18 -0300

    W2V model with fixation duration learning

---
+++ scripts/w2v_fix.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional
+from torch.nn import init
+
+"""
+    u_embedding: Embedding for center word.
+    v_embedding: Embedding for neighbor words.
+"""
+
+
+class SkipGram(nn.Module):
+
+    def __init__(self, emb_size, emb_dimension):
+        super(SkipGram, self).__init__()
+        self.emb_size = emb_size
+        self.emb_dimension = emb_dimension
+        self.u_embeddings = nn.Embedding(emb_size, emb_dimension, sparse=True)
+        self.v_embeddings = nn.Embedding(emb_size, emb_dimension, sparse=True)
+        self.duration_regression = nn.Linear(emb_dimension, 1)
+
+        initrange = 1.0 / self.emb_dimension
+        init.uniform_(self.u_embeddings.weight.data, -initrange, initrange)
+        init.constant_(self.v_embeddings.weight.data, 0)
+
+    def forward(self, pos_u, pos_v, neg_v):
+        emb_u = self.u_embeddings(pos_u)
+        emb_v = self.v_embeddings(pos_v)
+        emb_neg_v = self.v_embeddings(neg_v)
+
+        score = torch.sum(torch.mul(emb_u, emb_v), dim=1)
+        score = torch.clamp(score, max=10, min=-10)
+        score = -functional.logsigmoid(score)
+
+        neg_score = torch.bmm(emb_neg_v, emb_u.unsqueeze(2)).squeeze()
+        neg_score = torch.clamp(neg_score, max=10, min=-10)
+        neg_score = -torch.sum(functional.logsigmoid(-neg_score), dim=1)
+
+        duration = self.duration_regression(emb_v).squeeze()
+
+        return torch.mean(score + neg_score), duration
+
+    def save_embedding_vocab(self, vocab, file_name):
+        embedding = self.u_embeddings.weight.cpu().data.numpy()
+        with open(file_name, 'w') as f:
+            f.write('%d %d\n' % (len(vocab), self.emb_dimension))
+            for wid, w in enumerate(vocab.get_itos()):
+                e = ' '.join(map(lambda x: str(x), embedding[wid]))
+                f.write('%s %s\n' % (w, e))
