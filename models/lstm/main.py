f530a18afcf48c60b2fe033b44e3ac39524b7bb7
FerminT <fermintravi@gmail.com>
2024-06-06 11:59:53 -0300

    Merge lstm

--- models/lstm/main.py
+++ models/lstm/main.py
@@ -15,13 +15,15 @@ from scripts.corpora import Corpora
 from scripts.data_handling import build_vocab
 from scripts.plot import plot_loss, plot_ppl
 
+
 class AwdLSTM:
     SEED = 12345
-    
-    def __init__(self, corpora, name, save_path, layer_num = 3, embed_size = 400, hidden_size = 1150, lstm_type = "pytorch", 
-                 w_drop = 0.5, dropout_i = 0.4, dropout_l = 0.3, dropout_o = 0.4, dropout_e = 0.1, winit = 0.1,
-                 batch_size = 40, valid_batch_size = 10, bptt = 70, ar = 2, tar = 1, weight_decay = 1.2e-6, 
-                 epochs = 750, lr = 30, max_grad_norm = 0.25, non_mono = 5, device = "gpu", log = 100, min_word_count = 5, max_vocab_size = None ):
+
+    def __init__(self, corpora, name, save_path, layer_num=3, embed_size=400, hidden_size=1150, lstm_type="pytorch",
+                 w_drop=0.5, dropout_i=0.4, dropout_l=0.3, dropout_o=0.4, dropout_e=0.1, winit=0.1,
+                 batch_size=40, valid_batch_size=10, bptt=70, ar=2, tar=1, weight_decay=1.2e-6,
+                 epochs=750, lr=30, max_grad_norm=0.25, non_mono=5, device="gpu", log=5000, min_word_count=5,
+                 max_vocab_size=None):
         self.corpora = corpora
         self.name = name
         self.save_path = save_path
@@ -50,14 +52,14 @@ class AwdLSTM:
         self.min_word_count = min_word_count
         self.max_vocab_size = max_vocab_size
         torch.autograd.set_detect_anomaly(True)
-        
+
         self.set_device()
-        
+
         self.save_path.mkdir(exist_ok=True, parents=True)
         self.log_file = self.set_log_file()
-        
+
         self.data_init()
-        
+
     def set_device(self):
         if self.device == "gpu" and torch.cuda.is_available():
             print("Model will be training on the GPU.\n")
@@ -68,45 +70,45 @@ class AwdLSTM:
         else:
             print("Model will be training on the CPU.\n")
             self.device = torch.device('cpu')
-            
+
     def set_log_file(self):
         log = open(str(self.save_path / f'{self.name}.csv'), "w")
         log.write("epoch,valid_ppl\n")
         return log
-        
+
     def save_model(self, model):
         torch.save({'model_state_dict': model.state_dict()}, str(self.save_path / f'{self.name}.tar'))
-        
+
     def data_init(self):
         text = self.corpora
         split = text.corpora.train_test_split(test_size=0.2, seed=self.SEED)
-        
+
         trn = split['train']
         vld = split['test']
-            
+
         vocab = build_vocab(trn, self.min_word_count, self.max_vocab_size)[0]
-            
+
         print("Numericalizing Training set")
         trn = trn.map(lambda row: {"text": vocab.forward(row["text"]), "fix_dur": row["fix_dur"]}, num_proc=12)
         trn_tokens = [token for text in tqdm(trn['text']) for token in text]
         trn_fix = [token for text in tqdm(trn['fix_dur']) for token in text]
-        
+
         print("Numericalizing Validation set")
         vld = vld.map(lambda row: {"text": vocab.forward(row["text"]), "fix_dur": row["fix_dur"]}, num_proc=12)
         vld_tokens = [token for text in tqdm(vld['text']) for token in text]
         vld_fix = [token for text in tqdm(vld['fix_dur']) for token in text]
-        
-        self.trn_tokens = torch.tensor(trn_tokens,dtype=torch.int64).reshape(-1, 1)
-        self.trn_fix = torch.tensor(trn_fix,dtype=torch.int64).reshape(-1, 1)
-        self.vld_tokens = torch.tensor(vld_tokens,dtype=torch.int64).reshape(-1, 1)
-        self.vld_fix = torch.tensor(vld_fix,dtype=torch.int64).reshape(-1, 1)
+
+        self.trn_tokens = torch.tensor(trn_tokens, dtype=torch.int64).reshape(-1, 1)
+        self.trn_fix = torch.tensor(trn_fix, dtype=torch.int64).reshape(-1, 1)
+        self.vld_tokens = torch.tensor(vld_tokens, dtype=torch.int64).reshape(-1, 1)
+        self.vld_fix = torch.tensor(vld_fix, dtype=torch.int64).reshape(-1, 1)
         self.vocab = vocab.get_stoi()
 
     def get_seq_len(self):
-        seq_len = self.bptt if np.random.random() < 0.95 else self.bptt/2
+        seq_len = self.bptt if np.random.random() < 0.95 else self.bptt / 2
         seq_len = round(np.random.normal(seq_len, 5))
         while seq_len <= 5 or seq_len >= 90:
-            seq_len = self.bptt if np.random.random() < 0.95 else self.bptt/2
+            seq_len = self.bptt if np.random.random() < 0.95 else self.bptt / 2
             seq_len = round(np.random.normal(seq_len, 5))
         return seq_len
 
@@ -119,11 +121,11 @@ class AwdLSTM:
     def minibatch(self, data, fix_data, seq_length):
         num_batches = data.size(0)
         dataset = []
-        for i in range(0, num_batches-1, seq_length):
-            ls = min(i+seq_length, num_batches-1)
-            x = data[i:ls,:]
-            fix_x = fix_data[i:ls,:]
-            y = data[i+1:ls+1,:]
+        for i in range(0, num_batches - 1, seq_length):
+            ls = min(i + seq_length, num_batches - 1)
+            x = data[i:ls, :]
+            fix_x = fix_data[i:ls, :]
+            y = data[i + 1:ls + 1, :]
             dataset.append((x, y, fix_x))
         return dataset
 
@@ -141,7 +143,7 @@ class AwdLSTM:
                 loss = F.cross_entropy(scores, y.reshape(-1))
                 losses.append(loss.data.item())
         return np.exp(np.mean(losses))
-    
+
     def perplexity_for_training(self, batches, model):
         model.eval()
         with torch.no_grad():
@@ -155,7 +157,7 @@ class AwdLSTM:
                 loss = F.cross_entropy(scores, y.reshape(-1))
                 losses.append(loss.data.item())
         return np.exp(np.mean(losses))
-        
+
     def train_model(self, model, optimizer):
         tic = timeit.default_timer()
         total_words = 0
@@ -163,12 +165,12 @@ class AwdLSTM:
         best_val = 1e10
         loss_sg, loss_fix, ppl = [], [], {}
         for epoch in range(self.epochs):
-            print("Epoch : {:d}".format(epoch+1))
+            print("Epoch : {:d}".format(epoch + 1))
             seq_len = self.get_seq_len()
             optimizer.lr(seq_len / self.bptt * self.lr)
             states = model.state_init(self.batch_size)
             model.train()
-            
+
             minibatches = self.minibatch(self.trn_tokens, self.trn_fix, seq_len)
             minibatches_for_trn = random.sample(minibatches, int(len(minibatches) * 0.1))
             for i, (x, y, fix) in tqdm(enumerate(minibatches), desc="Training", total=len(minibatches)):
@@ -178,65 +180,64 @@ class AwdLSTM:
                 total_words += x.numel()
                 states = model.detach(states)
                 scores, states, activations, fix_pred = model(x, states)
-                
+
                 loss = F.cross_entropy(scores, y.reshape(-1))
                 h, h_m = activations
                 ar_reg = self.ar * h_m.pow(2).mean()
                 tar_reg = self.tar * (h[:-1] - h[1:]).pow(2).mean()
-                
+
                 if fix.sum() > 0:
                     fix_loss = torch.nn.L1Loss()(fix_pred, fix.reshape(-1))
                 else:
                     fix_loss = torch.tensor(0.0)
-                
+
                 loss_reg = loss + ar_reg + tar_reg + fix_loss
                 loss_reg.backward()
-                
+
                 loss_sg.append(loss.item() + ar_reg.item() + tar_reg.item())
                 loss_fix.append(fix_loss.item())
-                
+
                 nn.utils.clip_grad_norm_(model.parameters(), self.max_grad_norm)
                 optimizer.step()
                 optimizer.zero_grad()
-                
+
                 if i % self.log == 0:
                     ppl[i] = self.perplexity_for_training(minibatches_for_trn, model)
                     model.train()
-                    
-    
+
             tmp = {}
-            for (prm,st) in optimizer.state.items():
+            for (prm, st) in optimizer.state.items():
                 tmp[prm] = prm.clone().detach()
                 prm.data = st['ax'].clone().detach()
-    
+
             val_perp = self.perplexity(self.vld_tokens, self.vld_fix, model)
             optimizer.check(val_perp)
 
-            self.log_file.write("{:d},{:.3f}\n".format(epoch+1, val_perp))
-    
+            self.log_file.write("{:d},{:.3f}\n".format(epoch + 1, val_perp))
+
             if val_perp < best_val:
                 best_val = val_perp
                 print("Best validation perplexity : {:.3f}".format(best_val))
                 self.save_model(model)
                 print("Model saved!")
-    
+
             for (prm, st) in optimizer.state.items():
                 prm.data = tmp[prm].clone().detach()
-                
+
             toc = timeit.default_timer()
             print("Validation set perplexity : {:.3f}".format(val_perp))
             print("Since beginning : {:.3f} mins".format(round((toc - tic) / 60)))
             print("lr : {:.3f}".format(seq_len / self.bptt * self.lr))
-            print("*************************************************\n")        
+            print("*************************************************\n")
         self.log_file.close()
         plot_loss(loss_sg, loss_fix, self.name, self.save_path)
         plot_ppl(ppl, self.name, self.save_path)
         self.generate_embeddings(model)
-        
+
     def generate_embeddings(self, model):
         weights = model.embed.W
         vocabulary = OrderedDict(sorted(self.vocab.items(), key=lambda x: x[1]))
-        
+
         with open(str(self.save_path / f'{self.name}.vec'), "w") as f:
             f.write(f"{weights.shape[0]} {weights.shape[1]}\n")
             for _, (word, vector) in enumerate(zip(vocabulary.keys(), weights)):
@@ -247,11 +248,12 @@ class AwdLSTM:
         warnings.filterwarnings("ignore")
         self.trn_tokens, self.trn_fix = self.batchify(self.trn_tokens, self.trn_fix, self.batch_size)
         self.vld_tokens, self.vld_fix = self.batchify(self.vld_tokens, self.vld_fix, self.valid_batch_size)
-        model = Model(len(self.vocab), self.embed_size, self.hidden_size, self.layer_num, self.w_drop, self.dropout_i, 
-                    self.dropout_l, self.dropout_o, self.dropout_e, self.winit, self.lstm_type)
+        model = Model(len(self.vocab), self.embed_size, self.hidden_size, self.layer_num, self.w_drop, self.dropout_i,
+                      self.dropout_l, self.dropout_o, self.dropout_e, self.winit, self.lstm_type)
         model.to(self.device)
-        optimizer = NTASGD(model.parameters(), lr=self.lr, n=self.non_mono, weight_decay=self.weight_decay, fine_tuning=False)
+        optimizer = NTASGD(model.parameters(), lr=self.lr, n=self.non_mono, weight_decay=self.weight_decay,
+                           fine_tuning=False)
         self.train_model(model, optimizer)
 
-#model = AWD_LSTM_Model("pytorch_spanish_spacy_30000", epochs=200)
-#model.train()
+# model = AWD_LSTM_Model("pytorch_spanish_spacy_30000", epochs=200)
+# model.train()
